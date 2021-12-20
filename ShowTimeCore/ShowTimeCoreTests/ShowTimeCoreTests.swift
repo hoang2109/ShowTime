e03b96460b399d71452bd4645f9d214791b89c45
Hoang Nguyen <hoang21091989@gmail.com>
2021-12-20 14:09:07 +0800

    Create empty project

---
+++ ShowTimeCore/ShowTimeCoreTests/ShowTimeCoreTests.swift
@@ -0,0 +1,14 @@
+//
+//  ShowTimeCoreTests.swift
+//  ShowTimeCoreTests
+//
+//  Created by Hoang Nguyen on 20/12/21.
+//
+
+import XCTest
+
+class ShowTimeCoreTests: XCTestCase {
+    func test() {
+        
+    }
+}
